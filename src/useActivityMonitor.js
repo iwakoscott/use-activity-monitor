b6cbe629e3e73bf2de6dc1890df688da3031b9cd
Satoshi Iwako <iwakoscott@gmail.com>
2021-01-09 11:32:31 -0800

    chore: base commit

---
+++ src/useActivityMonitor.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+
+export const useActivityMonitor = (
+  callback,
+  {
+    when,
+    wait = 5000,
+    events = ["click", "mousemove", "mousedown", "keypress"]
+  } = {}
+) => {
+  useEffect(() => {
+    let timeout;
+    function resetTimer() {
+      clearTimeout(timeout);
+      timeout = setTimeout(callback, wait);
+    }
+
+    function clearTimer() {
+      clearTimeout(timeout);
+    }
+
+    if (!when) return;
+
+    resetTimer();
+
+    for (const event of events) {
+      document.addEventListener(event, resetTimer);
+    }
+    return () => {
+      clearTimer();
+      for (const event of events) {
+        document.removeEventListener(event, resetTimer);
+      }
+    };
+  }, [events, when, wait, callback]);
+};
